a95c60fd33ac65bae27a9fda51420591a1c54496
wojciechrut <wojciechrut1998@gmail.com>
2023-02-25 12:39:00 +0100

    Postgres connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'nestjs-practice',
+    username: 'postgres',
+    password: '2254045',
+    synchronize: true,
+    entities: [],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
